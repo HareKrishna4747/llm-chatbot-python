82f738520700b8423d51b4f73164fec5922a3023
Jayaditya Arora <j81691@gmail.com>
2025-01-07 01:24:14 +0530

    Initial commit

--- graph.py
+++ graph.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 # Connect to Neo4j
+import os
+from dotenv import load_dotenv
+
+
+# tag::graph[]
+from langchain_neo4j import Neo4jGraph
+
+graph = Neo4jGraph(
+    url="neo4j+s://d9c46fd0.databases.neo4j.io",
+    username="neo4j",
+    password="lqvXmMRMadUfXDtcSPs1W-FS7korC8_xs4GaXct91eY"
+)
+
+#end::graph[]
--- llm.py
+++ llm.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
 
-# Create the LLM
 
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+import json
 
-# Create the Embedding model
+embeddings = ChatGroq(
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.7,
+    api_key="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"
+)
+
+# Initialize Groq LLM
+llm = ChatGroq(
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.7,
+    api_key="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"
+)
